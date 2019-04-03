0fdecbce3ccce6fd8d6573f3ea20ec1d345f3bde
khj8308 <khj_8308@naver.com>
2019-04-03 18:41:50 +0900

    Create MNIST_test.py

---
+++ ch03/MNIST_test.py
@@ -0,0 +1,12 @@
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가텨올 수 있도록 설정
+from dataset.mnist import load_mnist
+
+#처음 한 번은 몇 분 정도 걸립니다.
+(x_train, t_train), (x_test, t_test) = \
+    load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
